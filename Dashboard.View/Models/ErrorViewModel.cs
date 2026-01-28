69359083a94eb6a9feb2154fba37f45625162ce1
hashmimuhammad <mhashimkust@gmail.com>
2026-01-28 21:49:10 +0500

    Initial commit

---
+++ Dashboard.View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dashboard.View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
